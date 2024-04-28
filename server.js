99ac44bb00d20b296416d4d635d9e915ee0655e2
Almohsen <50508250+ALLMOHSEN@users.noreply.github.com>
2024-04-28 22:03:24 +0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+const users = {};
+
+io.on('connection', (socket) => {
+    console.log('New user connected');
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+        delete users[socket.id];
+        io.emit('updateUserList', getUserList());
+    });
+
+    socket.on('join', (username) => {
+        users[socket.id] = username;
+        io.emit('updateUserList', getUserList());
+    });
+
+    socket.on('call', (data) => {
+        socket.to(data.targetId).emit('incomingCall', { caller: users[socket.id] });
+    });
+});
+
+function getUserList() {
+    return Object.values(users);
+}
+
+server.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
